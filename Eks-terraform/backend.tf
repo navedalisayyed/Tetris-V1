811e4bb0c0b83e6008f027225a83f8e89e7f8ba7
navedalisayyed <87929557+navedalisayyed@users.noreply.github.com>
2024-02-24 16:44:56 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "tetris-app-jenkins" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
 }
