0df532294bce2e0d62666a701a05923dc832e1ba
navedalisayyed <87929557+navedalisayyed@users.noreply.github.com>
2024-02-26 16:03:27 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -1,14 +1,13 @@
 terraform {
-   required_version = "~> 1.7.0"
-   required_providers {
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.30.0"
-     }
-   }
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  
 }
